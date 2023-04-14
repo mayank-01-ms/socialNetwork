76be26d9f42ec4539b8ffeb58772a680dc3bcb47
Mayank <mayank.singh@oncehub.com>
2023-04-14 12:08:03 +0530

    Added unique file name in uploader

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -41,7 +41,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    random_token = Digest::SHA2.hexdigest("#{Time.now.utc}--#{model.id.to_s}").first(20)
+    ivar = "@#{mounted_as}_secure_token"    
+    token = model.instance_variable_get(ivar)
+    token ||= model.instance_variable_set(ivar, random_token)
+    "#{model.id}_#{token}.jpg" if original_filename
+  end
 end
